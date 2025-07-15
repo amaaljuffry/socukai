945c7822da295a46afc2f8c052f8664cdd5b7d8c
Ama Aljuffry <amaaljuffry@gmail.com>
2025-07-15 18:26:43 +0800

    update photo link for meta

--- app/head.tsx
+++ app/head.tsx
@@ -1,22 +1,21 @@
 export default function Head() {
   return (
     <>
-      <title>Socukai.my</title>
+      <title>Socukai.my </title>
       <meta name="description" content="An info app Malaysian tax compliance." />
       {/* Facebook Meta Tags */}
-      <meta property="og:url" content="https://socukai.ama24.my/" />
+      <meta property="og:url" content="https://socukai.ama24.my/individual" />
       <meta property="og:type" content="website" />
-      <meta property="og:title" content="Socukai.my" />
+      <meta property="og:title" content="Socukai.my " />
       <meta property="og:description" content="An info app Malaysian tax compliance." />
-      <meta property="og:image" content="https://socukai.ama24.my/opengraphpreview.jpg" />
-      <meta property="og:logo" content="https://socukai.ama24.my/logo.svg" />
+      <meta property="og:image" content="https://opengraph.b-cdn.net/production/images/4006c82b-a620-4b1c-8e1c-26564d1e3912.jpg?token=fTmznL-EBokYeiZcGWhFBuaTeFlq2RcMrP2pd65Vl5M&height=630&width=1200&expires=33288575129" />
       {/* Twitter Meta Tags */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta property="twitter:domain" content="socukai.ama24.my" />
-      <meta property="twitter:url" content="https://socukai.ama24.my/" />
-      <meta name="twitter:title" content="Socukai.my" />
+      <meta property="twitter:url" content="https://socukai.ama24.my/individual" />
+      <meta name="twitter:title" content="Socukai.my " />
       <meta name="twitter:description" content="An info app Malaysian tax compliance." />
-      <meta name="twitter:image" content="https://socukai.ama24.my/opengraphpreview.jpg" />
+      <meta name="twitter:image" content="https://opengraph.b-cdn.net/production/images/4006c82b-a620-4b1c-8e1c-26564d1e3912.jpg?token=fTmznL-EBokYeiZcGWhFBuaTeFlq2RcMrP2pd65Vl5M&height=630&width=1200&expires=33288575129" />
     </>
   );
 } 
