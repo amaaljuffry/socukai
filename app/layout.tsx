13905ade1dd2418c7588ed7f3d3ea65055930485
Ama Aljuffry <amaaljuffry@gmail.com>
2025-07-15 18:14:34 +0800

    add Open Graph Meta Photo

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,14 +28,14 @@ export default function RootLayout({
         <meta property="og:type" content="website" />
         <meta property="og:title" content="Socukai.my " />
         <meta property="og:description" content="An info app Malaysian tax compliance." />
-        <meta property="og:image" content="/META-OPENGRAPH.jpg" />
+        <meta property="og:image" content="https://socukai.ama24.my/META-OPENGRAPH.jpg" />
         {/* Twitter Meta Tags */}
         <meta name="twitter:card" content="summary_large_image" />
         <meta property="twitter:domain" content="socukai.ama24.my" />
         <meta property="twitter:url" content="https://socukai.ama24.my/changelog" />
         <meta name="twitter:title" content="Socukai.my " />
         <meta name="twitter:description" content="An info app Malaysian tax compliance." />
-        <meta name="twitter:image" content="/META-OPENGRAPH.jpg" />
+        <meta name="twitter:image" content="https://socukai.ama24.my/META-OPENGRAPH.jpg" />
       </Head>
       <body className={inter.className + " min-h-screen flex flex-col overflow-x-hidden"}>
         {children}
