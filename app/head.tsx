4ece3a019484bdd48359f7c5805078f31748dacb
Ama Aljuffry <amaaljuffry@gmail.com>
2025-07-15 18:23:18 +0800

    update head page

--- app/head.tsx
+++ app/head.tsx
@@ -4,15 +4,16 @@ export default function Head() {
       <title>Socukai.my</title>
       <meta name="description" content="An info app Malaysian tax compliance." />
       {/* Facebook Meta Tags */}
-      <meta property="og:url" content="https://socukai.ama24.my/changelog" />
+      <meta property="og:url" content="https://socukai.ama24.my/" />
       <meta property="og:type" content="website" />
       <meta property="og:title" content="Socukai.my" />
       <meta property="og:description" content="An info app Malaysian tax compliance." />
       <meta property="og:image" content="https://socukai.ama24.my/opengraphpreview.jpg" />
+      <meta property="og:logo" content="https://socukai.ama24.my/logo.svg" />
       {/* Twitter Meta Tags */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta property="twitter:domain" content="socukai.ama24.my" />
-      <meta property="twitter:url" content="https://socukai.ama24.my/changelog" />
+      <meta property="twitter:url" content="https://socukai.ama24.my/" />
       <meta name="twitter:title" content="Socukai.my" />
       <meta name="twitter:description" content="An info app Malaysian tax compliance." />
       <meta name="twitter:image" content="https://socukai.ama24.my/opengraphpreview.jpg" />
